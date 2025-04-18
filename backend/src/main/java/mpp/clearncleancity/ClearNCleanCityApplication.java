ebba0027a852b63a4265e62543553b02a2f48e71
danhepai <socialmedia.dangaspar@gmail.com>
2025-04-18 23:32:36 +0300

    Init backend

---
+++ backend/src/main/java/mpp/clearncleancity/ClearNCleanCityApplication.java
@@ -0,0 +1,13 @@
+package mpp.clearncleancity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearNCleanCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearNCleanCityApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mpp/clearncleancity/ClearNCleanCityApplicationTests.java
@@ -0,0 +1,13 @@
+package mpp.clearncleancity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearNCleanCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
