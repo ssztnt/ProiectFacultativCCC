ddd6789131b48854ee52022359af8eb291212af8
danhepai <socialmedia.dangaspar@gmail.com>
2025-06-03 01:23:51 +0300

    Issue count API

--- backend/src/main/java/mpp/clearncleancity/repository/IssueRepository.java
+++ backend/src/main/java/mpp/clearncleancity/repository/IssueRepository.java
@@ -5,6 +5,7 @@ import mpp.clearncleancity.model.entitites.User;
 import mpp.clearncleancity.model.entitites.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -24,4 +25,7 @@ public interface IssueRepository extends JpaRepository<Issue, Long> {
 
     @Query("SELECT i FROM Issue i ORDER BY i.status ASC")
     List<Issue> findAllSortedByStatus();
+
+    @Query("SELECT COUNT(i) FROM Issue i WHERE i.user.id = :id")
+    Integer findIssueCount(@Param("id") Long id);
 }
